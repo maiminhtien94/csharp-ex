c9a035ba5074573353f9263919fb99e4990f2764
maiminhtien94 <maiminhtien1994@gmail.com>
2021-06-27 08:06:17 +0700

    The first push

---
+++ control-ex/picturebox-draw/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace picturebox_draw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptDraw = new System.Windows.Forms.PictureBox();
+            this.btCircle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptDraw
+            // 
+            this.ptDraw.BackColor = System.Drawing.Color.Black;
+            this.ptDraw.Location = new System.Drawing.Point(42, 33);
+            this.ptDraw.Name = "ptDraw";
+            this.ptDraw.Size = new System.Drawing.Size(812, 454);
+            this.ptDraw.TabIndex = 0;
+            this.ptDraw.TabStop = false;
+            // 
+            // btCircle
+            // 
+            this.btCircle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCircle.Location = new System.Drawing.Point(990, 53);
+            this.btCircle.Name = "btCircle";
+            this.btCircle.Size = new System.Drawing.Size(126, 78);
+            this.btCircle.TabIndex = 1;
+            this.btCircle.Text = "Line";
+            this.btCircle.UseVisualStyleBackColor = true;
+            this.btCircle.Click += new System.EventHandler(this.btCircle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 583);
+            this.Controls.Add(this.btCircle);
+            this.Controls.Add(this.ptDraw);
+            this.Name = "Form1";
+            this.Text = "Drawing Form";
+            ((System.ComponentModel.ISupportInitialize)(this.ptDraw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptDraw;
+        private System.Windows.Forms.Button btCircle;
+    }
+}
+
---
+++ control-ex/picturebox-draw/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace picturebox_draw
+{
+    public partial class Form1 : Form
+    {
+        struct Coordinate
+        {
+            public double X;
+            public double Y;
+
+            public Coordinate(double X, double Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+        }
+        public double hs_a=0, hs_b=0;
+        Coordinate A =new Coordinate (100, 30 );
+        Coordinate B = new Coordinate(20, 200);
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            Graphics g = ptDraw.CreateGraphics();
+            Pen myPen = new Pen(Color.Yellow, 2);
+            g.DrawLine(myPen, (float)(A.X), (float)(A.Y), (float)(B.X), (float)(B.Y));
+            equation_line();
+            Console.WriteLine("A (" + A.X + "," + A.Y + ")");
+            Console.WriteLine("B (" + B.X + "," + B.Y + ")");
+            Console.WriteLine("a=" + hs_a);
+            Console.WriteLine("b=" + hs_b);
+            Console.WriteLine("(107,15) check:" + ((107 * hs_a + hs_b)-15));
+        }
+
+        public void equation_line()
+        { 
+            hs_a=(A.Y - B.Y)/(A.X-B.X);
+            hs_b = (B.Y * A.X - B.X * A.Y) / (A.X - B.X); 
+        }
+
+        public void tinhtiendoanthang()
+        {
+            
+            A.X = A.X + 20;
+            B.X = B.X + 20;
+            equation_line();
+
+            Console.WriteLine("A_offset (" + A.X + "," + A.Y+")");
+            Console.WriteLine("B_offset (" + B.X + "," + B.Y+")");
+            Console.WriteLine("hs:" + hs_a);
+            Console.WriteLine("hs:" + hs_b);
+            Console.WriteLine("Ay check:" + (A.X * hs_a + hs_b));
+            Console.WriteLine("By check:" + (B.X * hs_a + hs_b));
+
+        }
+
+        private void btCircle_Click(object sender, EventArgs e)
+        {
+            
+            Graphics g = ptDraw.CreateGraphics();
+            Pen myPen = new Pen(Color.Yellow, 2);
+            if ((A.X < ptDraw.Size.Width && B.X < ptDraw.Size.Width) && (A.Y < ptDraw.Size.Height && B.Y < ptDraw.Size.Height))
+            {
+                tinhtiendoanthang();
+                g.DrawLine(myPen, (float)(A.X), (float)(A.Y), (float)(B.X), (float)(B.Y));
+            }
+        }
+
+    }
+}
